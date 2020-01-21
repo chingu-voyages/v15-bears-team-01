14f26026a088da3c9c911ca4e83118af51237908
Will Gutierrez <will.gutierrez@gmail.com>
2020-01-21 14:42:09 -0800

    modify header so depending on auth it displays different items

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,9 +1,12 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
 
-import StyledHeader from './Header.styles';
+import AuthContext from "../../utils/auth_context";
+
+import StyledHeader from "./Header.styles";
 
 const Header = () => {
+  const context = useContext(AuthContext);
 
   return (
     <StyledHeader.Container className="app-header">
@@ -14,14 +17,27 @@ const Header = () => {
         <nav>
           <ul>
             <li>
-              <StyledHeader.NavItem to="/about">About</StyledHeader.NavItem>
+              <Link to="/about">About</Link>
             </li>
-            <li>
-              <StyledHeader.NavItem to="/profile">Profile</StyledHeader.NavItem>
-            </li>
-            <li>
-              <StyledHeader.NavItem to="/login">Login</StyledHeader.NavItem>
-            </li>
+            {context.state.isAuthenticated && (
+              <li>
+                <Link to="/profile">
+                  Profile
+                </Link>
+              </li>
+            )}
+            {!context.state.isAuthenticated && (
+              <li>
+                <Link to="/login">Login</Link>
+              </li>
+            )}
+            {context.state.isAuthenticated && (
+              <li>
+                <StyledHeader.NavItem onClick={context.LogOut}>
+                  Logout
+                </StyledHeader.NavItem>
+              </li>
+            )}
           </ul>
         </nav>
       </StyledHeader.Navigation>
